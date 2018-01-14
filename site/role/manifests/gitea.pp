5e58b4125939c2f4b48803c0cd7d38aa57face96
andrewm3 <2037905+andrewm3@users.noreply.github.com>
2018-01-15 10:50:21 +1100

    Add minimal gitea and jenkins role - only base profile

---
+++ site/role/manifests/gitea.pp
@@ -0,0 +1,11 @@
+# role::gitea
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include role::gitea
+class role::gitea {
+  require profile::base
+}
---
+++ site/role/manifests/jenkins.pp
@@ -0,0 +1,11 @@
+# role::jenkins
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include role::jenkins
+class role::jenkins {
+  require profile::base
+}
