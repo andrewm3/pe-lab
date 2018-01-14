0df2047178256a0b4f5758b1ab6acb663dad1bdf
andrewm3 <2037905+andrewm3@users.noreply.github.com>
2018-01-15 10:57:01 +1100

    Add compile masters, load balancer, gitea and jenkins instances

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,3 +31,73 @@ EOF
     "sudo /opt/puppetlabs/bin/puppet-code deploy --all --wait",
   ]
 }
+
+module "puppet-compile-1" {
+  source = "github.com/andrewm3/terraform-openstack-puppet-enterprise"
+
+  name = "puppet-compile-1"
+  key_pair = "${openstack_compute_keypair_v2.terraform.name}"
+  network_uuid = "${openstack_networking_network_v2.terraform.id}"
+  flavor = "m1.medium"
+  pool = "${var.pool}"
+  pp_role = "puppet::compile_master"
+  node_type = "compile-master"
+  master_ip = "${local.primary_master_ip}"
+  master_hostname = "puppet-master-primary"
+}
+
+module "puppet-compile-2" {
+  source = "github.com/andrewm3/terraform-openstack-puppet-enterprise"
+
+  name = "puppet-compile-2"
+  key_pair = "${openstack_compute_keypair_v2.terraform.name}"
+  network_uuid = "${openstack_networking_network_v2.terraform.id}"
+  flavor = "m1.medium"
+  pool = "${var.pool}"
+  pp_role = "puppet::compile_master"
+  node_type = "compile-master"
+  master_ip = "${local.primary_master_ip}"
+  master_hostname = "puppet-master-primary"
+}
+
+module "load-balancer" {
+  source = "github.com/andrewm3/terraform-openstack-puppet-enterprise"
+
+  name = "load-balancer"
+  key_pair = "${openstack_compute_keypair_v2.terraform.name}"
+  network_uuid = "${openstack_networking_network_v2.terraform.id}"
+  flavor = "g1.small"
+  pool = "${var.pool}"
+  pp_role = "load_balancer"
+  node_type = "posix-agent"
+  master_ip = "${local.primary_master_ip}"
+  master_hostname = "puppet-master-primary"
+}
+
+module "gitea" {
+  source = "github.com/andrewm3/terraform-openstack-puppet-enterprise"
+
+  name = "gitea"
+  key_pair = "${openstack_compute_keypair_v2.terraform.name}"
+  network_uuid = "${openstack_networking_network_v2.terraform.id}"
+  flavor = "g1.small"
+  pool = "${var.pool}"
+  pp_role = "gitea"
+  node_type = "posix-agent"
+  master_ip = "${local.primary_master_ip}"
+  master_hostname = "puppet-master-primary"
+}
+
+module "jenkins" {
+  source = "github.com/andrewm3/terraform-openstack-puppet-enterprise"
+
+  name = "jenkins"
+  key_pair = "${openstack_compute_keypair_v2.terraform.name}"
+  network_uuid = "${openstack_networking_network_v2.terraform.id}"
+  flavor = "g1.medium"
+  pool = "${var.pool}"
+  pp_role = "jenkins"
+  node_type = "posix-agent"
+  master_ip = "${local.primary_master_ip}"
+  master_hostname = "puppet-master-primary"
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,11 @@
 output "primary_master_ip" {
   value = "${module.puppet-master-primary.public_ip}"
 }
+
+output "gitea_ip" {
+  value = "${module.gitea.public_ip}"
+}
+
+output "jenkins_ip" {
+  value = "${module.jenkins.public_ip}"
+}
