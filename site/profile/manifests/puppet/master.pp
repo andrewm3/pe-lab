66d81cb65d20b977c64827fa882ec005beff2fe0
andrewm3 <2037905+andrewm3@users.noreply.github.com>
2018-01-15 15:47:48 +1100

    Add management of PE Master node group, drop include of pe_repo::platform classes in master profile

--- site/profile/manifests/puppet/master.pp
+++ site/profile/manifests/puppet/master.pp
@@ -6,10 +6,7 @@
 #   include profile::puppet::master
 #
 class profile::puppet::master (
-  Array[String] $pe_repo_platforms = [],
 ) {
 
-  $pe_repo_platforms.each |$pe_repo_platform| {
-    include "pe_repo::platform::${pe_repo_platform}"
-  }
+  # There is nothing here.
 }
