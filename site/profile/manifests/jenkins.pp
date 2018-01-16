2a794403b26ae4eff93c707f1ed84d8c431e0df0
andrewm3 <2037905+andrewm3@users.noreply.github.com>
2018-01-16 14:00:21 +1100

    Add management of Jenkins jobs

--- site/profile/manifests/jenkins.pp
+++ site/profile/manifests/jenkins.pp
@@ -7,6 +7,7 @@
 #
 class profile::jenkins (
   Hash[String, String] $plugin_versions = {},
+  Hash[String, String] $job_files       = {},
 ) {
 
   include ::git
@@ -17,6 +18,13 @@ class profile::jenkins (
   $plugin_versions.each |$plugin_name, $plugin_version| {
     jenkins::plugin { $plugin_name:
       version => $plugin_version,
+    }
+  }
+
+  # Configure Jenkins Pipeline jobs from files
+  $job_files.each |$job_name, $job_file| {
+    jenkins::job { $job_name:
+      config => file($job_file),
     }
   }
 }
