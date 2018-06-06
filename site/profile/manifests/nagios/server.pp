fca9e09ca3a0dbb7231a6d63634bffe4462046ee
andrewm3 <2037905+andrewm3@users.noreply.github.com>
2018-06-06 15:45:59 +1000

    Manage required configuration in nagios server profile

--- site/profile/manifests/nagios/server.pp
+++ site/profile/manifests/nagios/server.pp
@@ -18,4 +18,37 @@ class profile::nagios::server {
   package { ['nagios', 'nagios-plugins', 'nagios-plugins-all']:
     ensure => present,
   }
+
+  file { '/etc/nagios/conf.d':
+    ensure  => directory,
+    owner   => 'nagios',
+    group   => 'nagios',
+    mode    => '0755',
+    require => Package['nagios'],
+  }
+
+  # apache config for nagios
+  file { '/etc/httpd/conf.d/nagios.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => epp('profile/nagios/nagios.conf.epp'),
+    require => Package['nagios'],
+  }
+
+  service { 'nagios':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/etc/httpd/conf.d/nagios.conf'],
+  }
+
+  firewall { '100 Nagios HTTP 80':
+    dport  => '80',
+    proto  => 'tcp',
+    action => 'accept',
+  }
+
+  Nagios_host <<||>>
+  Nagios_service <<||>>
 }
